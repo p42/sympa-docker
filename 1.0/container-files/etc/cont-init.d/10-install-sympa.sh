70c7818c1d09decea84809c315b045818eb59730
Rance Hall <ranceh@gmail.com>
2017-08-29 14:58:50 -0500

    container files layout

---
+++ 1.0/container-files/etc/cont-init.d/10-install-sympa.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/with-contenv /bin/bash
+
+set -e
+
+#Apply environment variables to config files where needed.
+
+# SYMPA_DOMAIN
+if [[ "$SYMPA_DOMAIN" !== "lists.mydomain.com" ]]; then
+	sed -e "s/SYMPA_DOMAIN/$SYMPA_DOMAIN/g" /etc/nginx/conf.d/sympa.conf > /etc/nginx/conf.d/sympa.conf.new
+  sed -e "s/SYMPA_DOMAIN/$SYMPA_DOMAIN/g" /etc/sympa/sympa.conf > /etc/sympa/sympa.conf.new
+	mv /etc/nginx/conf.d/sympa.conf.new /etc/nginx/conf.d/sympa.conf
+	mv /etc/sympa/sympa.conf.new /etc/sympa/sympa.conf
+fi
+
+#set db type in sympa config file
+sed -e "s/SYMPA_DB_TYPE/$SYMPA_DB_TYPE/g" /etc/sympa/sympa.conf > /etc/sympa/sympa.conf.new
+mv /etc/sympa/sympa.conf.new /etc/sympa/sympa.conf
+
+#set db host in sympa config file
+sed -e "s/SYMPA_DB_HOST/$SYMPA_DB_HOST/g" /etc/sympa/sympa.conf > /etc/sympa/sympa.conf.new
+mv /etc/sympa/sympa.conf.new /etc/sympa/sympa.conf
+
+#set db user in sympa config file
+sed -e "s/SYMPA_DB_USER/$SYMPA_DB_USER/g" /etc/sympa/sympa.conf > /etc/sympa/sympa.conf.new
+mv /etc/sympa/sympa.conf.new /etc/sympa/sympa.conf
+
+#set db password in sympa config file
+sed -e "s/SYMPA_DB_PASS/$SYMPA_DB_PASS/g" /etc/sympa/sympa.conf > /etc/sympa/sympa.conf.new
+mv /etc/sympa/sympa.conf.new /etc/sympa/sympa.conf
+
+#set db name in sympa config file
+sed -e "s/SYMPA_DB_NAME/$SYMPA_DB_NAME/g" /etc/sympa/sympa.conf > /etc/sympa/sympa.conf.new
+mv /etc/sympa/sympa.conf.new /etc/sympa/sympa.conf
+
+#set sympa listmasters in sympa config file
+sed -e "s/SYMPA_LISTMASTERS/$SYMPA_LISTMASTERS/g" /etc/sympa/sympa.conf > /etc/sympa/sympa.conf.new
+mv /etc/sympa/sympa.conf.new /etc/sympa/sympa.conf
+
+# apply relayhost to postfix config if requested.
+
+if [[ "SYMPA_POSTFIX_RELAY" !== "mail.mydomain.com" ]]; then
+	sed -e "s/#relayhost = SYMPA_POSTFIX_RELAY/relayhost = $SYMPA_POSTFIX_RELAY/g" /etc/postfix/main.cf > /etc/postfix/main.cf.new
+	mv /etc/postfix/main.cf.new /etc/postfix/main.cf
+fi
+
+# Configuration complete
+
+# run data migration
+
+# look for the breadcrumb file placed by the
+if [ ! -f /do_not_remove ]; then
+
+	#Create a breadcrumb file
+	echo "Presence of this file will prevent execution of the recover portion of the init script in case the container is recreated." > /do_not_remove
+
+fi
