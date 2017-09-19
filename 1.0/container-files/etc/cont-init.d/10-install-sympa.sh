8072350f698c50e1891f82901588641af7c6eb6a
Rance Hall <ranceh@gmail.com>
2017-09-19 15:10:34 -0500

    change name of presympa script

--- 1.0/container-files/etc/cont-init.d/10-install-sympa.sh
+++ 1.0/container-files/etc/cont-init.d/10-install-sympa.sh
@@ -48,6 +48,6 @@ if [ "$SYMPA_REMOTE_LOG_SERVER" != "logger.mydomain.com" ]; then
 	mv /etc/rsyslog.conf.new /etc/rsyslog.conf
 fi
 
-chmod +x /usr/sbin/presympa.#!/bin/sh
+chmod +x /usr/sbin/presympa
 
 # Configuration complete
